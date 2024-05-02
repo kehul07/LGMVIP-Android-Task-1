75e5b03173cd82d63cc2ee0a6ee701f84b660901
kehul07 <kehulsali2004@gmail.com>
2024-05-02 18:19:50 +0530

    final commit

--- app/src/main/java/com/kehuldroid/covid19tracker/DistrictActivity.java
+++ app/src/main/java/com/kehuldroid/covid19tracker/DistrictActivity.java
@@ -2,22 +2,18 @@ package com.kehuldroid.covid19tracker;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
